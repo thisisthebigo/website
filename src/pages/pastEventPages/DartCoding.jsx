e9fc6a9e4bf583c23dd250eab023ade462c3764a
Anugrah-Singh <anugrahsinngh@gmail.com>
2025-03-07 08:41:59 +0530

    comment out registration links in past event pages

--- src/pages/pastEventPages/DartCoding.jsx
+++ src/pages/pastEventPages/DartCoding.jsx
@@ -320,7 +320,7 @@ const DartCoding = () => {
                             Back to Home
                         </button>
                     </Link>
-                    <a
+                    {/* <a
                         href="https://docs.google.com/forms/d/e/1FAIpQLSfteSmmlB3VKZpPIY8AVrIhNd38FfTxuWpf7fA2TSQG5Wgifg/viewform"
                         onMouseEnter={(e) => e.target.style.transform = 'scale(1.05)'}
                         onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
@@ -328,7 +328,7 @@ const DartCoding = () => {
                         <button style={styles.gradientButton}>
                             Register Now
                         </button>
-                    </a>
+                    </a> */}
                 </div>
 
                 {/* Key Info Grid */}
